f313561cb2186ade68b3757521423c0e6bbe8583
cchuster <creativechu@outlook.com>
2021-10-16 11:28:31 -0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'John',
+  name: 'Conner',
   imageBackground: false,
   openInNewTab: true,
   twelveHourFormat: false,
@@ -20,17 +20,17 @@ const CONFIG = {
   greetingMorning: 'Good morning!',
   greetingAfternoon: 'Good afternoon,',
   greetingEvening: 'Good evening,',
-  greetingNight: 'Go to Sleep!',
+  greetingNight: 'Good night!',
 
   // Weather
-  weatherKey: 'f7f5f52f032c84d50403c302e460c28e',
+  weatherKey: 'InsertYourAPIKeyHere123456',
   weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
-  weatherUnit: 'F', // 'F', 'C'
+  weatherUnit: 'C', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: '34.149490',
-  defaultLongitude: '-118.439957',
+  defaultLatitude: '37.775',
+  defaultLongitude: '-122.419',
 
   // ┌─┐┌─┐┬─┐┌┬┐┌─┐
   // │  ├─┤├┬┘ ││└─┐
@@ -40,86 +40,86 @@ const CONFIG = {
   cards: [
     {
       id: '1',
-      name: 'Github',
-      icon: 'github',
-      link: 'https://github.com/',
+      name: 'Outlook',
+      icon: 'mail',
+     link: 'https://outlook.live.com/mail/0/inbox',
     },
     {
       id: '2',
-      name: 'Mail',
-      icon: 'mail',
-      link: 'https://mail.protonmail.com/',
-    },
-    {
-      id: '3',
-      name: 'Todoist',
-      icon: 'trello',
-      link: 'https://calendar.google.com/calendar/r',
-    },
-    {
-      id: '4',
-      name: 'Calendar',
-      icon: 'calendar',
-      link: 'https://calendar.google.com/calendar/r',
+      name: 'Gmail',
+      icon: 'inbox',
+      link: 'https://mail.google.com/mail/u/1/#inbox',
     },
     {
       id: '5',
-      name: 'Reddit',
-      icon: 'bookmark',
-      link: 'https://reddit.com',
+      name: 'LinkedIn',
+      icon: 'linkedin',
+      link: 'https://linkedin.com/',
+    },
+    {
+      id: '3',
+      name: 'Schoology',
+      icon: 'briefcase',
+      link: 'https://lms.lausd.net/home',
+    },
+    {
+      id: '4',
+      name: 'Google Docs',
+      icon: 'file-text',
+      link: 'https://docs.google.com/document/u/1/',
     },
     {
       id: '6',
-      name: 'Odysee',
-      icon: 'youtube',
-      link: 'https://odysee.com/',
+      name: 'Google Sheets',
+      icon: 'layout',
+      link: 'https://docs.google.com/spreadsheets/u/0/',
     },
   ],
-
+  
   // ┬  ┬┌─┐┌┬┐┌─┐
   // │  │└─┐ │ └─┐
   // ┴─┘┴└─┘ ┴ └─┘
 
   //Icons
-  firstListIcon: 'music',
+  firstListIcon: 'send',
   secondListIcon: 'coffee',
 
   // Links
   lists: {
     firstList: [
       {
-        name: 'Inspirational',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'Webull',
+        link: 'https://www.webull.com/',
       },
       {
-        name: 'Classic',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'TelePAWS',
+        link: 'https://www.telepaws.com/',
       },
       {
-        name: 'Oldies',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'CPU Advantage',
+        link: 'https://www.cpua.org/',
       },
       {
-        name: 'Rock',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'Gainsboro',
+        link: 'https://www.gainsboro.tech/',
       },
     ],
     secondList: [
       {
-        name: 'Linkedin',
-        link: 'https://linkedin.com/',
+        name: 'Spotify',
+        link: 'https://open.spotify.com/playlist/3Jo6FyvoZwGLWQPA0d9dGD',
       },
       {
-        name: 'Figma',
-        link: 'https://figma.com/',
+        name: 'YouTube',
+        link: 'http://youtube.com/',
       },
       {
-        name: 'Dribbble',
-        link: 'https://dribbble.com',
+        name: 'Reddit',
+        link: 'https://www.reddit.com/',
       },
       {
-        name: 'Telegram',
-        link: 'https://webk.telegram.org',
+        name: 'LookMovie',
+        link: 'https://lookmovie.io/',
       },
     ],
   },
